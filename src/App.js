418ff2d089e6d5d582107a950de47878dce2b87b
Pabloanm <pabloanm@yahoo.com>
2022-08-11 01:23:39 -0300

    array with movies

--- src/App.js
+++ src/App.js
@@ -4,42 +4,51 @@ import Movie from "./movie";
 import PageWrapper from "./pagewrapper";
 
 function App() {
-  return (
 
+  let peliculas = [
+	{
+		titulo: "oblivion (2012)",
+        calificacion: "8.1",
+        mmpa: "PG-13",
+        runtime: "2h21’",
+        director:"Joss Whedon",
+        actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+        release: "1 May 2015",
+        urltitle: "moviesingle.html",
+        img:"images/uploads/mv1.jpg",
+		description:"Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
+	},
+	{
+		titulo: "into the wild(2014)",
+        calificacion: "7.8",
+        mmpa: "PG-13",
+        runtime: "2h21’",
+        director:"Anthony Russo, Joe Russo",
+        actores: "Chris Evans, Samuel L. Jackson, Scarlett Johansson",
+        release: "1 May 2015",
+        urltitle: "moviesingle.html",
+        img:"images/uploads/mv2.jpg",
+		description:"Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
+	}	
+  ]
+
+  return (
     <PageWrapper>
 
-      <Movie
-        titulo="oblivion (2012)"
-        calificacion="8.1"
-        mmpa="PG-13"
-        runtime="2h21’"
-        director="Joss Whedon"
-        actores="Robert Downey Jr., Chris Evans, Chris Hemsworth"
-        release="1 May 2015"
-        urltitle="moviesingle.html"
-        img="images/uploads/mv1.jpg"
-      >
-        Earth's mightiest heroes must come together and learn to fight as a team
-        if they are to stop the mischievous Loki and his alien army from
-        enslaving humanity...
+	{peliculas.map(pelicula => 
+       <Movie
+        titulo={pelicula.titulo}
+        calificacion={pelicula.calificacion}
+        mmpa={pelicula.mmpa}
+        runtime={pelicula.runtime}
+        director={pelicula.director}
+        actores={pelicula.actores}
+        release={pelicula.release}
+        urltitle={pelicula.urltitle}
+        img={pelicula.img}> 
+		description={pelicula.description}>    
       </Movie>
-
-      <Movie
-        titulo="into the wild(2014)"
-        calificacion="7.8"
-        mmpa="PG-13"
-        runtime="2h21’"
-        director="Anthony Russo, Joe Russo"
-        actores="Chris Evans, Samuel L. Jackson, Scarlett Johansson"
-        release="1 May 2015"
-        urltitle="moviesingle.html"
-        img="images/uploads/mv2.jpg"
->
-        As Steve Rogers struggles to embrace his role in the modern world, he
-        teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to
-        battle a new threat...
-      </Movie>
-
+		)}
     </PageWrapper>
 
   );
