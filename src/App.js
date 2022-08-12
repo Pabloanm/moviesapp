7062bcdede7078cfc4bd3bb7e4c8b6f055f9ffdd
Pabloanm <pabloanm@yahoo.com>
2022-08-12 01:52:18 -0300

    component search movie

--- src/App.js
+++ src/App.js
@@ -4,10 +4,48 @@ import Movie from "./movie";
 import moviesJson from "./movies.json";
 import PageWrapper from "./pagewrapper";
 import Pagination from "./pagination";
+import { useState } from 'react';
 
 function App() {
 
+  //[var paginaActual, function setPaginaActual to modify paginaActual]	
+  const [paginaActual, setPaginaActual] = useState(1);
+  const TOTAL_POR_PAGINA = 7;
+
   let peliculas = moviesJson; //import Json with movies
+
+  const buscarPelicula = async () => {
+	let url = ''; //aqui va el url	
+
+	//request to server
+	var respuesta = await fetch(url,{
+		"method": 'GET',
+		"headers": {
+			"Accept": 'application/json',
+			"Content-Type": 'application/json'
+		}
+	});
+	
+	//paso el objeto del fetch a formato json
+	var json = await respuesta.json();
+
+	alert(resultado);
+  }
+
+
+  const cargarPeliculas = () => {
+    peliculas = peliculas.slice(
+      (paginaActual - 1) * TOTAL_POR_PAGINA,
+      paginaActual * TOTAL_POR_PAGINA
+    );
+  }
+
+  const getTotalPaginas = () => {
+    let cantidadTotalDePeliculas = moviesJson.length;
+    return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA);
+  }
+
+  cargarPeliculas();
 
   return (
     <PageWrapper>
@@ -29,11 +67,11 @@ function App() {
       {/* Pagina actual / cantidad de paginas / acción o evento al cambiar de pagina */}
 
       <Pagination
-        pagina={1}
-        total={4}
+        pagina={paginaActual}
+        total={getTotalPaginas()}
         onChange={(pagina) => {
-          alert(pagina)
-        }}
+			setPaginaActual(pagina)
+		  }}
       />
     </PageWrapper>
   );
